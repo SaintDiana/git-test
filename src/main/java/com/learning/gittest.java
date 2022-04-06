5e5a2ad3d95cea990850151a2c1e9f7812630118
SaintDiana <89449894+SaintDiana@users.noreply.github.com>
2022-04-06 22:58:27 +0800

    Update gittest.java

--- src/main/java/com/learning/gittest.java
+++ src/main/java/com/learning/gittest.java
@@ -6,6 +6,6 @@ public class gittest {
         System.out.println("hello git");
         System.out.println("push test!");
           System.out.println("pull test");
-        System.out.println("ssh ssh");
+        System.out.println("ssh ssh s");
     }
 }
