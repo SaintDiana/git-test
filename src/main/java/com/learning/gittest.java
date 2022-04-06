7abf8895b389dd3647789f7209c854de0dbb4e09
yyh <1318870170@qq.com>
2022-04-06 21:54:22 +0800

    first commit

---
+++ src/main/java/com/learning/gittest.java
@@ -0,0 +1,8 @@
+package com.learning;
+
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("AVaVaAVa");
+        System.out.println("hello git");
+    }
+}
